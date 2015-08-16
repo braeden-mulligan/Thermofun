ecca7008513c6e7787c0bf0c54ebabff4009cb5f
Braeden Mulligan <braeden.mulligan@gmail.com>
2015-08-15 20:17:00 -0700

    fixed the fixed

--- thermostat/thermostat.py
+++ thermostat/thermostat.py
@@ -1,3 +1,4 @@
+# Thermostat controller
 
 gpioStop = open("/sys/class/gpio/unexport/", 'w')
 gpioStop.write("17")
@@ -12,10 +13,14 @@ gpioInit.close()
 gpio17 = open("/sys/class/gpio/gpio17/direction", 'w')
 gpio17.write("out")
 gpio17.close()
-
 gpio27 = open("/sys/class/gpio/gpio27/direction", 'w')
 gpio27.write("out")
 gpio27.close()
+
+while( 1 ){
+	sleep(10)
+	#do temperature stuff
+}
 
 # gpio17 = open("/sys/class/gpio/gpio17/value", 'w')
 # gpio27 = open("/sys/class/gpio/gpio27/value", 'w')
