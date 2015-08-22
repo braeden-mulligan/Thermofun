79785ed932b3b47fe8951d282eb4bdab3c1a191a
Braeden Mulligan <braeden.mulligan@gmail.com>
2015-08-22 15:14:41 -0700

    done

--- thermostat/thermostat.py
+++ thermostat/thermostat.py
@@ -1,45 +1,65 @@
 #!/usr/bin/env python
 # Thermostat controller
 
-def clearPins():
-	try:
-		gpioStop = open("/sys/class/gpio/unexport/", 'w')
-		gpioStop.write("17")
-		gpioStop.write("27")
-		gpioStop.close()
-		return 0
-	except:
-		return 1
+import time
+#import RPi.GPIO as GPIO
 
-def initPins():
-	try:
-		gpioInit = open("/sys/class/gpio/export", 'w')
-		gpioInit.write("17")
-		gpioInit.write("27")
-		gpioInit.close()
+def switchOn():
+#	GPIO.output(27, 1)
+	time.sleep(0.265)
+#	GPIO.output(27, 0)
 
-		gpioType = open("/sys/class/gpio/gpio17/direction", 'w')
-		gpioType.write("out")
-		gpioType.close()
-		gpioType = open("/sys/class/gpio/gpio27/direction", 'w')
-		gpioType.write("out")
-		gpioType.close()
-		return 0
-	except:
-		return 1
+def switchOff():
+#	GPIO.output(17, 1)
+	time.sleep(0.265)
+#	GPIO.output(17, 0)
 
-def switch(pin, state):
-	try:
-		gpio = open("/sys/class/gpio/gpio"+str(pin)+"/value", 'w')
-		gpio.write("{}".format(state))
-		return 0
-	except:
-		gpio.write("{}".format(0))
-		return 1
+if __name__ == "__main__":
+#	GPIO.setmode(GPIO.BCM)
+#	GPIO.setup(27, GPIO.OUT)
+#	GPIO.setup(17, GPIO.OUT)
 
-def main():
-	# switch(12, 22)
+	enable = 0
+	cutoff = 25.0
+	target = 21.0
+	thresh_high = target + 1.0
+	thresh_low = target - 1.0
 
-main()
+	thermo = 20.0
+	active = 0
 
+	while True:
+		with open("/sys/bus/w1/devices/28-0000054bcb79/w1_slave", 'r') as poll:
+			measure = poll.readline()
+			if measure.split()[11] == "YES":
+				measure = poll.readline()
+				thermo = ((float)(measure.split("t=")[1]))/1000
 
+		with open("../settings/settings.yml", 'r') as settings:
+			for line in settings:
+				if line.split(": ")[0] == "enabled":
+					enable = line.split(": ")[1]
+				elif line.split(": ")[0] == "max_temp":
+					cutoff = line.split(": ")[1]
+				elif line.split(": ")[0] == "target_temp":
+					target = line.split(": ")[1]
+		thresh_high = target + 1.0
+		thresh_low = target - 1.0
+
+		if (thermo > cutoff) or (enable == 0) :
+			if active == 1:
+				switchOff()
+				active = 0
+			time.sleep(30)
+		else:
+			if (thermo < thresh_low) and (active == 0):
+				switchOn()
+				active = 1
+			elif (thermo > thresh_high) and (active == 1):
+				switchOff()
+				active = 0
+			
+			time.sleep(30)
+
+	# Only useful if user can set a parameter to exit the loop
+#	GPIO.cleanup()
