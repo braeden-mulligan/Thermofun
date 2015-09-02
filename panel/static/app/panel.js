2cdde38ac3e2314bf88c19cb8889463dcb5adb3a
John Zwiep <jzwiep@gmail.com>
2015-09-02 12:58:07 -0700

    added prototype ui

--- panel/static/app/panel.js
+++ panel/static/app/panel.js
@@ -1,6 +1,23 @@
+'use strict';
+
 var panel = angular.module('panel', []);
 
 panel.controller('mainController', function($scope) {
+
+  $scope.enabled = true;
+  $scope.targetTemp = 27;
+
+  $scope.increaseTemp = function () {
     $scope.enabled = true;
-    $scope.temperature = '27';
+    $scope.targetTemp = $scope.targetTemp + 1;
+  };
+
+  $scope.decreaseTemp = function () {
+    $scope.enabled = true;
+    $scope.targetTemp = $scope.targetTemp - 1;
+  };
+
+  $scope.toggleEnabled = function () {
+    $scope.enabled = !$scope.enabled;
+  };
 });
